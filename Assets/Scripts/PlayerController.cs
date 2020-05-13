5927a7a3addc92852a3d110648440565b9c5afbd
Darek Desu <darekdesu@gmail.com>
2020-05-13 21:16:09 +0200

    New Project with Primitives

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float horizontalInput;
+    public float verticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+       transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
+       transform.Translate(Vector3.left * verticalInput * Time.deltaTime * speed);
+    }
+}
