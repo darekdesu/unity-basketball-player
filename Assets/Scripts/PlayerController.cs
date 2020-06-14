d31c453446307f6db38d623320fe439f26e4884f
Darek Desu <darekdesu@gmail.com>
2020-06-14 23:44:41 +0200

    Added ability to catch ball by player
    When player hit the ball, ball is automatically attached to the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 10f;
     public float horizontalInput;
     public float verticalInput;
-    
+
+    GameObject ballInHand;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +21,26 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * verticalInput * Time.deltaTime * speed);
 
-       transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
-       transform.Translate(Vector3.left * verticalInput * Time.deltaTime * speed);
+        if (ballInHand != null)
+        {
+            ballInHand.transform.parent = gameObject.transform;
+            ballInHand.transform.localPosition = new Vector3(0, 0, 1.2f);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision enter: " + other.gameObject.name);
+
+        if (other.gameObject.CompareTag("BallTag"))
+            ballInHand = other.gameObject;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        Debug.Log("Collision exit: " + other.gameObject.name);
     }
 }
---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 initialPosition;
+    Quaternion initialRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10f)
+        {
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+        }
+    }
+}
